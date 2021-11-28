b2e4142e1dc75dbc5697a20a97087fc84ea1d137
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-28 23:17:54 +0600

    Saving to GitHub

--- CRUD_Create_POST/api/views.py
+++ CRUD_Create_POST/api/views.py
@@ -1,3 +1,27 @@
+import json
 from django.shortcuts import render
-
+import io
+from rest_framework.parsers import JSONParser
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 # Create your views here.
+
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id', None)
+        
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type = 'application/json')
+        
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many = True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type = 'application/json')
